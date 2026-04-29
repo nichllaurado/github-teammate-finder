041d642a86979cba7f377329f6cfaa78d8fc5cab
nichllaurado <n1llaurado@gmail.com>
2026-04-29 12:44:53 -0400

    ui

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Information Retrieval & Web Agents Final Project
 Nicholas Llaurado
+
+run with python -m app.main
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv
 from app.routes import bp
@@ -8,9 +8,14 @@ load_dotenv()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     CORS(app)
     app.register_blueprint(bp, url_prefix="/api")
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
     return app
 
 
